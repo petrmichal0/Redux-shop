6adbffe38c497e95e794a2e98737b33ce1f615a4
Petr Michal <petrmichal0@gmail.com>
2024-03-21 22:15:54 +0100

    first commit

---
+++ src/store/hooks.ts
@@ -0,0 +1,11 @@
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { AppDispatch, RootState } from "./store";
+
+type DispatchFunction = () => AppDispatch;
+
+export const useCartDispatch: DispatchFunction = useDispatch;
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
